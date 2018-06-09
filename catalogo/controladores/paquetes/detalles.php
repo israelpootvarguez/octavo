e16a0ec7c74e00eb9e1d265e75838b9fcb902658
David Poot <david.poot@softstart.mx>
2018-06-09 18:57:30 -0500

    productos y detalles

--- catalogo/controladores/paquetes/detalles.php
+++ catalogo/controladores/paquetes/detalles.php
@@ -1,12 +1,17 @@
 <?php
 
+$id = tomarParametro("id","GET",NULL);
+//pacPackagesDescriptions_NameSEO
+
 if(($l == "ES") || ($l == "Es") || ($l == "es") ){
   $txt_informacion = array(
     'miga1' =>'paquetes/es',
+    'miga2' =>'paquetes/es',
   );
 }else{
   $txt_informacion = array(
     'miga1' =>'paquetes/en',
+    'miga2' =>'paquetes/en',
   );
 }
 
@@ -14,11 +19,13 @@ $sql = "SELECT pp.*, ppd.* FROM pacPackages pp
 LEFT JOIN pacPackagesDescriptions ppd on(pp.pacPackages_Id = ppd.pacPackages_Id)
 LEFT JOIN sysIdioms si on(si.sysIdioms_Id = ppd.sysIdioms_Id)
 WHERE (
-  si.sysIdioms_Code = '{$l}' 
+  ppd.pacPackagesDescriptions_NameSEO = '{$id}' 
   and pp.pacPackages_IsPublic = 1
 )";
 
 $productos = consultarSql($sql);
+if(count($productos) == 0){header('Location: http://www.adpromex.com'); print_r("fallo");}else{
+print_r($productos);  
+}
 
-print_r($productos);
 ?>
